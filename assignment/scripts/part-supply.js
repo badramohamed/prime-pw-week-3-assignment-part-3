2ffe988ef52475099241d49b98722b5f12a2638b
Badra Mohamed <moha1150@umn.edu>
2022-04-23 18:34:29 -0500

    fixed

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,6 +4,7 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
+
 let partsNeeded= 40
 console.log(partsNeeded);
 
@@ -22,13 +23,14 @@ console.log(supplyChanges [1]);
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
-console.log(supplyChanges [5]);
-supplyChanges.pop
+console.log(supplyChanges [4]);
+supplyChanges.pop();
+
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 
 supplyChanges.push[25];
-console.log(supplyChanges);
+
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
